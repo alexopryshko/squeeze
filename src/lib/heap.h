f53d873b63bbf40e58c2fba7229f61ef6f91ef67
Alex Opryshko <alexopryshko@yandex.ru>
2014-11-25 15:21:30 +0300

    refactoring

--- src/lib/heap.h
+++ src/lib/heap.h
@@ -1,63 +1,73 @@
 #pragma once
+
 #include <vector>
 #include <algorithm>
+#include <iostream>
 
 using std::vector;
 
-template <class el>
+template<class el>
 class heap {
 public:
     heap() {
         heapBuffer = new vector<el>();
     }
 
-    heap(vector<el>* heapBuffer) {
+    heap(vector<el> *heapBuffer) {
         this->heapBuffer = heapBuffer;
+        build();
     }
+
     void build();
+
     void add(el element);
+
     el extractMax();
-    bool isEmpty() { return heapBuffer->empty(); }
+
+    bool isEmpty() {
+        return heapBuffer->empty();
+    }
 
 private:
     void heapify(int i);
-    vector<el>* heapBuffer = nullptr;
+
+    vector<el> *heapBuffer = nullptr;
 };
 
 
-template <class el>
+template<class el>
 void heap<el>::heapify(int i) {
     int left = 2 * i + 1;
     int right = 2 * i + 2;
 
     int largest = i;
-    if( left < heapBuffer->size() && heapBuffer->at(left) > heapBuffer->at(i) )
+    if (left < heapBuffer->size() && *heapBuffer->at(left) > *heapBuffer->at(i))
         largest = left;
-    if( right < heapBuffer->size() && heapBuffer->at(right) > heapBuffer->at(largest) )
+    if (right < heapBuffer->size() && *heapBuffer->at(right) > *heapBuffer->at(largest))
         largest = right;
 
-    if( largest != i ) {
+    if (largest != i) {
         auto iter = heapBuffer->begin();
         iter_swap(iter + i, iter + largest);
         heapify(largest);
     }
 }
 
-template <class el>
+template<class el>
 void heap<el>::build() {
-    for( int i = heapBuffer->size() / 2 - 1; i >= 0; --i ) {
-        heapify( i );
+    for (int i = heapBuffer->size() / 2 - 1; i >= 0; --i) {
+        heapify(i);
     }
 }
 
-template <class el>
+template<class el>
 void heap<el>::add(el element) {
     heapBuffer->push_back(element);
     int i = heapBuffer->size() - 1;
 
     while (i > 0) {
-        int parent = (i - 1) /2;
-        if (heapBuffer->at(i) < heapBuffer->at(parent)) {
+        int parent = (i - 1) / 2;
+        if (*heapBuffer->at(i) < *heapBuffer->at(parent)) {
             return;
         }
         auto iter = heapBuffer->begin();
@@ -66,7 +76,7 @@ void heap<el>::add(el element) {
     }
 }
 
-template <class el>
+template<class el>
 el heap<el>::extractMax() {
     if (heapBuffer->empty()) {
         throw;
@@ -75,7 +85,7 @@ el heap<el>::extractMax() {
     auto iter = heapBuffer->begin();
     *iter = heapBuffer->at(heapBuffer->size() - 1);
     heapBuffer->pop_back();
-    if( !heapBuffer->empty() ) {
+    if (!heapBuffer->empty()) {
         heapify(0);
     }
     return result;
