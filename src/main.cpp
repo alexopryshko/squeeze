64f20be00b92e7123e4e1a1154a9c7a284d4fdbd
Alex Opryshko <alexopryshko@yandex.ru>
2015-01-05 01:55:54 +0300

    move main to src

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "squeeze.h"
+
+using namespace std;
+
+int main() {
+    //squeeze::compress("/Users/alexander/Development/squeeze/tests/", "test1.bmp", "/Users/alexander/Development/squeeze/tests/");
+    //squeeze::decompress("/Users/alexander/Development/squeeze/tests/", "test1.bmp.sqz", "/Users/alexander/Development/squeeze/tests/results/");
+    return 0;
+}
